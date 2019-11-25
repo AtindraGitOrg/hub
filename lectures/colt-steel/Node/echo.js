2c98fee4e589989a381f31f69e152ed73054c9b7
aashish <aashish@wm.com>
2019-11-25 16:45:25 +0530

    node

---
+++ lectures/colt-steel/Node/echo.js
@@ -0,0 +1,10 @@
+function echo(var1, var2) {
+    for (let index = 0; index < var2; index++) {
+        
+    console.log(var1);
+    }
+    
+}
+
+echo("Echo!!!",10);
+echo("Tater tots",3);
---
+++ lectures/colt-steel/Node/fakerU.js
@@ -0,0 +1,9 @@
+var faker = require('faker');
+
+var rName= faker.name.findName();
+
+var rproduct= faker.address.city();
+
+console.log(rName);
+
+console.log(rproduct);
