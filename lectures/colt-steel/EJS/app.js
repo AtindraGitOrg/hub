ace5296282426510373fefb3332d24ee58557f8c
aashish <aashish@wm.com>
2019-12-01 23:42:35 +0530

    enter the commit message for your changes. Lines starting

---
+++ lectures/colt-steel/EJS/app.js
@@ -0,0 +1,32 @@
+//import and object creation 
+
+var express = require('express');
+var app = express();
+
+
+
+//static file use
+app.use(express.static('public'));
+
+//view being set
+
+app.set('view engine', 'ejs');
+
+
+
+
+
+//route
+
+app.get('/',function(req,res){
+    //res.setHeader('Contant-Type','text/html')
+    res.render('imp',{userName:'',adult:''});
+    
+});
+
+app.get('/:name/:permission',function(req,res){
+    res.render('imp',{userName:req.params.name,adult:req.params.permission})
+    });
+
+
+app.listen(8080);
---
+++ lectures/colt-steel/HTML/operators.js
@@ -0,0 +1,5 @@
+if(0==false) {console.log('0==flase is true');}
+else {console.log('0==false is false');}
+
+console.log('abc');
+
